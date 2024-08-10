a57d25b00b4534d2192c19b822aa1650433abf3a
KillzN <scorpionsheep203@gmail.com>
2024-08-10 18:28:35 -0400

    KILL - BOT

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -13,11 +13,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`;
-  let teks = `*> Tʜᴇ Sᴜɴ Is Bʀɪɢʜᴛ Bᴜᴛ Nᴏᴛ As Bʀɪɢʜᴛ As Mᴇ ☀️*\n\n ${oi}\n\n➥ _*@ineffable.mvrco:*_\n`;
+  let teks = `*𝘼𝘾𝙏𝙄𝙑𝙀𝙉𝙎𝙀 𝙑𝙍𝙂𝙎 😝*\n\n ${oi}\n\n➥ _*Mencıón Generαl:*_\n`;
   for (const mem of participants) {
-    teks += `🥷🏼 ⇝ @${mem.id.split('@')[0]}\n`;
+    teks += `➤ ⭐ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└ *KɪʟʟBᴏᴛ ⇝@ineffable.mvrco*`;
+  teks += `└ *𝖬𝗂𝗍𝗌𝗎𝗋𝗂 𝖡𝗈𝗍 ⇝@kathypazff*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
